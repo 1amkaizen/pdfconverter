fc22dc65840f1fa3368d7cf176f437ac9978e34a
1amkaizen <1amkaiz3n@gmail.com>
2023-10-02 06:25:37 +0000

    first commit

---
+++ pdfconverter.go
@@ -0,0 +1,23 @@
+// pdfconverter.go
+
+package pdfconverter
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+// ConvertPDFToText converts a PDF file to text.
+func ConvertPDFToText(inputPDFPath, outputTextPath string) error {
+	cmd := exec.Command("pdftotext", inputPDFPath, outputTextPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error converting PDF to text: %v", err)
+	}
+
+	return nil
+}
