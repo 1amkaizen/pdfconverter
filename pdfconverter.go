1abf7663cc71e13ce7a87dbf012ac1d8dddc1235
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-10-03 23:23:53 +0700

    Update pdfconverter.go

--- pdfconverter.go
+++ pdfconverter.go
@@ -1,25 +1,21 @@
-// pdfconverter.go
-
 package pdfconverter
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
 )
 
-// ConvertPDFToText converts a PDF file to text and returns the text.
-func ConvertPDFToText(inputPDFPath string) (string, error) {
-	cmd := exec.Command("pdftotext", inputPDFPath, "-")
-	var outBuffer bytes.Buffer
-	cmd.Stdout = &outBuffer
-	cmd.Stderr = &outBuffer
+// ConvertPDFToText converts a PDF file to text.
+func ConvertPDFToText(inputPDFPath, outputTextPath string) error {
+	cmd := exec.Command("pdftotext", inputPDFPath, outputTextPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
 	err := cmd.Run()
 	if err != nil {
-		return "", fmt.Errorf("error converting PDF to text: %v", err)
+		return fmt.Errorf("error converting PDF to text: %v", err)
 	}
 
-	return outBuffer.String(), nil
+	return nil
 }
