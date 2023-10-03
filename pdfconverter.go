ffa29ccbe990eb141506df4c71a2cad59651b056
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-10-03 23:25:36 +0700

    Update pdfconverter.go

--- pdfconverter.go
+++ pdfconverter.go
@@ -7,8 +7,8 @@ import (
 )
 
 // ConvertPDFToText converts a PDF file to text.
-func ConvertPDFToText(inputPDFPath, outputTextPath string) error {
-	cmd := exec.Command("pdftotext", inputPDFPath, outputTextPath)
+func ConvertPDFToText(inputPDFPath string) error {
+	cmd := exec.Command("pdftotext", inputPDFPath, "-")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
