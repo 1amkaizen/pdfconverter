e819c04167ed83da9d2aedc89c12c9a261af0aac
1amkaizen <1amkaiz3n@gmail.com>
2023-10-04 16:14:15 +0000

    update

---
+++ features/feature.go
@@ -0,0 +1,10 @@
+// File: pdfconverter/features/feature.go
+
+package features
+
+import "fmt"
+
+func NewFeature() {
+    fmt.Println("This is a new feature!")
+}
+
--- pdfconverter.go
+++ pdfconverter.go
@@ -1,24 +1,28 @@
-
+// File: pdfconverter/pdfconverter.go
 
 package pdfconverter
 
 import (
-    "fmt"
-    "os"
-    "os/exec"
+	"fmt"
+	"os"
+	"os/exec"
+	"github.com/1amkaizen/pdfconverter/features" // Sesuaikan dengan path package yang sesuai
 )
 
 // ConvertPDFToText converts a PDF file to text.
 func ConvertPDFToText(inputPDFPath, outputTextPath string) error {
-    cmd := exec.Command("pdftotext", inputPDFPath, outputTextPath)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
+	cmd := exec.Command("pdftotext", inputPDFPath, outputTextPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-    err := cmd.Run()
-    if err != nil {
-        return fmt.Errorf("error converting PDF to text: %v", err)
-    }
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error converting PDF to text: %v", err)
+	}
 
-    return nil
+	// Call the new feature
+	features.NewFeature()
+
+	return nil
 }
 
