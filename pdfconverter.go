3522132df485c4bb05a576b2af02ff715dc9190d
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-10-04 22:59:13 +0700

    Update pdfconverter.go

--- pdfconverter.go
+++ pdfconverter.go
@@ -22,17 +22,3 @@ func ConvertPDFToText(inputPDFPath, outputTextPath string) error {
     return nil
 }
 
-
-// GetPDFMetadata retrieves metadata from a PDF file.
-func GetPDFMetadata(pdfPath string) error {
-    cmd := exec.Command("pdfinfo", pdfPath)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-
-    err := cmd.Run()
-    if err != nil {
-        return fmt.Errorf("error getting PDF metadata: %v", err)
-    }
-
-    return nil
-}
