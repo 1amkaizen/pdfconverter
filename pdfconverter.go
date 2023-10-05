53f6dd574ee517ff8431885244f05ecf721e1bc2
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-10-05 21:50:09 +0700

    Update pdfconverter.go

--- pdfconverter.go
+++ pdfconverter.go
@@ -22,15 +22,15 @@ func ConvertPDFToText(inputPDFPath, outputTextPath string) error {
 	return nil
 }
 
-// GetPDFMetadata retrieves metadata from a PDF file.
-func GetPDFMetadata(inputPDFPath string) error {
-	cmd := exec.Command("pdfinfo", inputPDFPath)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		return fmt.Errorf("error getting PDF metadata: %v", err)
-	}
+// ConvertTextToPDF converts a text file to PDF.
+func ConvertTextToPDF(inputTextPath, outputPDFPath string) error {
+    cmd := exec.Command("pdftotext", inputTextPath, outputPDFPath)
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    err := cmd.Run()
+    if err != nil {
+        return fmt.Errorf("error converting text to PDF: %v", err)
+    }
 
-	return nil
+    return nil
 }
